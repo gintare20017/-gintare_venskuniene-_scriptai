6d8a4fa4c7cacd5b28f77ddedf69508cb59691a0
gintare20017 <gintare20017@gmail.com>
2021-11-25 14:08:52 +0200

    pirma uzduotis

---
+++ failu-kurimas.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir uzduoties-info
+cd uzduoties-info
+touch uzduotis.txt
+cd..
+echo Šiandiena yra atliekama Linux bei SQL užduotis šio žmogaus: > uzduotis.txt
+cat uzduotis.txt ../inicialai.info > sujungtas.cat
+
+
